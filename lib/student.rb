34e3de4661c1c016b04e90549c1ffb64738453d0
Jason Viger <jason.viger@gmail.com>
2018-08-24 09:15:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,16 @@ class Student
   
   attr_accessor :name, :grade 
   
-  def create_table
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
     
   
 end
